f03a4cf9ff63072c4b0b72e224e136ecc894585d
Grim Reaper <htetoowaiyan002@gmail.com>
2022-09-02 01:21:44 -0700

    about component v1

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>This is About Section</h1>
+
+    <div class="container">
+        <h1>Hello</h1>
+        
+        <div class="content">
+            <img src="../assets/logo.png" alt="aboutImage" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus ad molestias nisi suscipit labore! A, nemo saepe quaerat, architecto officia exercitationem sed placeat dolor quis nihil repudiandae illo obcaecati repellendus.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur consectetur enim quidem doloribus at velit incidunt esse sed ipsa distinctio impedit ab, perspiciatis totam quisquam ex sequi, laudantium provident voluptate.
+            </p>
+        </div>
+
+        <div class="footer">
+
+            <p>Find Us On</p>
+            <icon :icon="['fab' , 'facebook' ]"/>
+            <icon :icon="['fab' , 'telegram' ]"/>
+
+        </div>
+    </div>
+
+
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faTelegram } from '@fortawesome/free-brands-svg-icons'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+library.add(faFacebook , faTelegram)
+
+createApp(App)
+.component('icon', FontAwesomeIcon)
+.use(router)
+.mount('#app')
