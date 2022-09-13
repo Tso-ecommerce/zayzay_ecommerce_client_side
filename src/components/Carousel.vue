51203ff71b0bcd9f32f2bb833a1b0784acdb87ec
B@C <aungthuyeinhein98@gmail.com>
2022-09-14 00:04:05 +0630

    added pagination still having assests error

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,15 +1,113 @@
 <template>
-    <hr/>
-  <h1>This is Banner Section</h1>
-  <hr/>
+  <div class="carousel">
+    <slot :currentSlide="currentSlide"/>
+
+    <!-- Navigation -->
+    <div class="navigate">
+      <div class="toogle-page left">
+        <i @click="prevSlide" class="fas fa-chevron-left"></i>
+      </div>
+
+      <div class="toogle-page right">
+        <i @click="nextSlide" class="fas fa-chevron-right"></i>
+      </div>
+
+      <!-- Pagination -->
+        <div class="pagination">
+          <span v-for="(slide,index) in getSlideCount" :key="index" :class="{active:index + 1 === currentSlide }">{{slide}}</span>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import {ref, onMounted} from "vue";
 
+export default {
+  setup() {
+    const currentSlide = ref(1);
+
+    const getSlideCount = ref(null);
+
+    //next slide
+
+    const nextSlide = ()=>{
+      if (currentSlide.value===getSlideCount.value){
+        currentSlide.value += 1;
+        return;
+      }
+      currentSlide.value +=1;
+    }
+
+    //prev slide
+
+    const prevSlide = ()=>{
+      if(currentSlide.value === 1) {
+        currentSlide.value = 1;
+        return;
+      }
+      currentSlide.value -= 1;
+    }
+
+    onMounted(()=>{
+      getSlideCount.value = document.querySelectorAll(".slide").length;
+    })
+
+    return {currentSlide,nextSlide,prevSlide,getSlideCount};
+  },
 }
 </script>
 
 <style>
+  .navigate{
+    padding: 0 16px;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+  }
 
+  .toggle-page{
+    display: flex;
+    flex: 1;
+  }
+
+  .right{
+    justify-content: flex-end;
+  }
+
+  .i{
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    border-radius: 50%;
+    width: 40%;
+    height: 40%;
+    background-color: #6347c7;
+    color: #fff;
+  }
+
+  .pagination{
+    position: absolute;
+    bottom : 24px;
+    width: 100%;
+    display: flex;
+    gap: 16px;
+    justify-content: center;
+    align-items: center;
+  }
+
+  span{
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #fff;
+    box-shadow: 0 1px 3px p rgba(0,0,0,0.1),0 1px 2px 0 rgba(0,0,0,0.06);
+  }
+  
+  .active{
+    background-color: #6347c7;
+  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <Carousel></Carousel>
+    <Carousel class="carousel" v-slot="{currentSlide}">
+      <Slide v-for="(slide,index) in carouselSlides" :key="index">
+        <div v-show="currentSlide === index +1" class="slide-info">
+          <img :src="required(`../img/${slide}.jpg`)" alt=""> 
+        </div>
+      </Slide>
+    </Carousel>
     <Searchbar></Searchbar>
     <Productslist></Productslist>
     <Cart></Cart>
@@ -11,6 +17,7 @@
 <script>
 // @ is an alias to /src
 import Carousel from '@/components/Carousel.vue'
+import Slide from '@/components/Slide.vue'
 import SearchBar from '@/components/Searchbar.vue'
 import Productslist from '@/components/Productslist.vue'
 import Cart from '@/components/Cart.vue'
@@ -23,11 +30,46 @@ export default {
   name: 'HomeView',
   components: {
     Carousel,
+    Slide,
     SearchBar,
     Productslist,
     Cart,
     About,
     Searchbar
-}
-}
+},
+
+
+
+setup() {
+  const carouselSlides = ["bg-1","bg-2","bg-3"];
+
+  return {carouselSlides};
+
+},
+};
 </script>
+
+<style scoped>
+  .carousel{
+    position: relative;
+    max-height:100vh;
+    height:100vh;
+  }
+
+  .slide-info{
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-height: 100%;
+    height: 100%;
+  }
+
+  img{
+    min-width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+    
+  
+</style>
